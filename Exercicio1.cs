b6bcc4bd4632cc2b7c551503930d5ce48bfc3436
Lua-da-tarde <luanna@paggiaro.com.br>
2022-03-23 15:41:19 -0300

    Exercicio 1 em C# 23/03

---
+++ Exercicio1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio1emCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 v1, v2;
+            Console.Write("Valor 1: ");
+            v1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Valor 2: ");
+            v2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Soma = {0}", (v1 + v2));
+        }
+    }
+}
