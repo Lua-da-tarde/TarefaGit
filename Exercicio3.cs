2b1549352e26197b73432f6a4bb1bdec9507c0c0
Lua-da-tarde <luanna@paggiaro.com.br>
2022-03-23 17:02:05 -0300

    Exercicio 3 em C# 23/03

---
+++ Exercicio3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio3emCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 a, b, c, d;
+            Console.Write("A : ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("B : ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.Write("C : ");
+            c = Int32.Parse(Console.ReadLine());
+            Console.Write("D : ");
+            d = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("DIFERENÇA = {0}", (a * b) - (c * d));
+        }
+    }
+}
