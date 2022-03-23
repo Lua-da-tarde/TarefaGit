9d6f94027f3b475ac36f799beb2bfb6618a2e148
Lua-da-tarde <luanna@paggiaro.com.br>
2022-03-23 16:48:04 -0300

    Exercicio 2 em C# 23/03

---
+++ Exercicio2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio2emCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double raio = 0.0;
+            Double area = 0.0;
+            Console.Write("Raio : ");
+            raio = Double.Parse(Console.ReadLine());
+            area = Math.PI * Math.Pow(raio, 2);
+            Console.WriteLine("Area : {0:F4}", area);
+        }
+    }
+}
